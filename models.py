da35ec919f41b627367f0bdd9ffd909460f14eb1
rekib0023 <rkb.ra0025@gmail.com>
2021-02-17 14:59:40 +0000

    task model correct

--- models.py
+++ models.py
@@ -18,6 +18,7 @@ class User(db.Model):
     creation_date = db.Column(
         db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False
     )
+    # tasks = db.relationship('Task', backref='user', lazy=True)
 
     def __init__(self, authKey, username, email, password):
         self.authKey = authKey
@@ -26,6 +27,38 @@ class User(db.Model):
         self.password = password
 
 
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    note = db.Column(db.String(250), unique=True, nullable=False)
+    completed = db.Column(db.Boolean(), default=False, nullable=False)
+    end_date = db.Column(db.TIMESTAMP, unique=True, nullable=False)
+    creation_date = db.Column(
+        db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False
+    )
+
+    def __init__(self, title, note, end_date):
+        self.title = authKey
+        self.note = note
+        self.end_date = end_date
+
+
+class TaskSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
+    title = fields.String(required=True)
+    note = fields.String(required=True)
+    completed = fields.Boolean(required=True)
+    end_date = fields.DateTime(required=True)
+    creation_date = fields.DateTime()
+
+
+
+    
 class UserSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     authKey = fields.String(required=True)
@@ -33,3 +66,4 @@ class UserSchema(ma.Schema):
     email = fields.String(required=True)
     password = fields.String(required=True)
     creation_date = fields.DateTime()
+    # tasks = fields.List(fields.Nested(TaskSchema))
